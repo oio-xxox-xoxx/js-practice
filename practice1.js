9c4b269218f498702acff66c4bc884ad3e8d98e2
oio-xxox-xoxx <mj7657@naver.com>
2024-03-08 22:35:23 +0900

    practice

---
+++ practice1.js
@@ -0,0 +1,31 @@
+let x, y, z;    
+x = 5;          
+y = 6;          
+z = x + y;      
+console.log(z);
+
+let a, b, c;  
+a = 5; b = 6; c = a + b;
+console.log(c);
+
+
+var q;
+let w;
+q = 5;
+w = 6;
+let e = (q + w) * 2;
+console.log(e);
+
+
+let name = "John" + " " + "Doe"
+console.log(name);
+
+let lastname, lastName;
+lastName = "Doe";
+lastname = "Peterson";
+console.log(lastName);
+console.log(lastname);
+
+
+
+
