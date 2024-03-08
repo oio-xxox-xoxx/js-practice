5e0823c87d1aa4123efd1d2148ed7062c1fc40b7
oio-xxox-xoxx <mj7657@naver.com>
2024-03-08 22:55:01 +0900

    practice2

---
+++ practice2.js
@@ -0,0 +1,23 @@
+{
+    let x = 2;
+    console.log(x);
+  }
+  // x can NOT be used here
+  console.log(x);
+
+
+  {
+    var x = 2;
+    console.log(x);
+  }
+  // x CAN be used here
+  console.log(x);
+
+
+
+
+let x = "John Doe";
+// x can NOT be used
+let x = 0;
+console.log(x);
+
