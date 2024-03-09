47ae7708ff1415fa98ceb8d89c99fb49d1e7762f
oio-xxox-xoxx <mj7657@naver.com>
2024-03-09 23:57:10 +0900

    practice6

---
+++ practice6.js
@@ -0,0 +1,18 @@
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
+console.log(result);
+
+let text3 = "20";
+let text4 = "5";
+let result2 = text1 < text2;
+console.log(result2);
+
+let text5 = "John";
+let text6 = "Doe";
+let text7 = text5 + " " + text6;
+console.log(text7);
+
+let text8 = "What a very ";
+text8 += "nice day";
+console.log(text8);
